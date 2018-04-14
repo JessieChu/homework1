0f49df64b39fee5721c0f85af846782ddaea13a3
JessieChu <zjqjessiechu@163.com>
2018-04-15 00:21:53 +0800

    no message

---
+++ main_data1.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import itertools
+import draw_pic
+import clean_statistic as cs
+
+# 读取csv文件
+def load_file(file):
+    csv_file = pd.read_csv(file, low_memory=False)
+    return csv_file
+
+
+if __name__ == "__main__":
+    homework_file1 = r"/Users/jessiechu/Documents/文档/DMPro/hw1/Data/Building_Permits.csv"
+
+    # 人工选择所有数值属性
+    numeric_attr1 = ['Number of Existing Stories', 'Number of Proposed Stories', 'Estimated Cost', 'Revised Cost',
+                    'Existing Units', 'Proposed Units']
+    csv_file1 = load_file(homework_file1)
+
+    cs.nominal_statistic(csv_file1, numeric_attr1, "Building_Permits")
+    cs.numeric_statistic(csv_file1, numeric_attr1, "Building_Permits")
+    draw_pic.draw_numeric(csv_file1, numeric_attr1)
+
+    # 绘制两个属性的qq图，判断相关性
+    for double_column in itertools.combinations(numeric_attr1, 2):
+        draw_pic.draw_qq_double(csv_file1, double_column)
+
+    # 只展示Estimated Cost属性填补后的效果
+    draw_pic.complete_dropna(csv_file1, 'Estimated Cost')
+    draw_pic.complete_fre_attr(csv_file1, 'Estimated Cost')
+    draw_pic.complete_rel_attr(csv_file1, ['Estimated Cost', 'Revised Cost'])
+    draw_pic.complete_smi_attr(csv_file1, 'Estimated Cost', numeric_attr1)
